85d23cc4472e0e08fbeeedea2f0daec57b1e6c3f
AaronPlave <aplave@wesleyan.edu>
2014-10-20 15:49:59 -0400

    testing cross domain issues

--- runserver.py
+++ runserver.py
@@ -1,2 +1,2 @@
 from wes_api import app
-app.run(debug=True, host='0.0.0.0', port=5000)
+app.run(debug=True, host='0.0.0.0', port=80)
--- wes_api/api.py
+++ wes_api/api.py
@@ -1,12 +1,61 @@
 from lib.search import search
-from flask import Blueprint, request
+from flask import Blueprint, request, Flask, make_response, request, current_app
 import json
 import time
 import datetime
+from datetime import timedelta
+from functools import update_wrapper
 
 api = Blueprint('api', __name__, template_folder='templates')
 EVENT_SOURCES = ["wesleying", "wesleyanEvents"]
 MENU_SOURCES = ["usdan"]
+
+"""
+http://flask.pocoo.org/snippets/56/
+"""
+
+
+def crossdomain(origin=None, methods=None, headers=None,
+                max_age=21600, attach_to_all=True,
+                automatic_options=True):
+    if methods is not None:
+        methods = ', '.join(sorted(x.upper() for x in methods))
+    if headers is not None and not isinstance(headers, basestring):
+        headers = ', '.join(x.upper() for x in headers)
+    if not isinstance(origin, basestring):
+        origin = ', '.join(origin)
+    if isinstance(max_age, timedelta):
+        max_age = max_age.total_seconds()
+
+    def get_methods():
+        if methods is not None:
+            return methods
+
+        options_resp = current_app.make_default_options_response()
+        return options_resp.headers['allow']
+
+    def decorator(f):
+        def wrapped_function(*args, **kwargs):
+            if automatic_options and request.method == 'OPTIONS':
+                resp = current_app.make_default_options_response()
+            else:
+                resp = make_response(f(*args, **kwargs))
+            if not attach_to_all and request.method != 'OPTIONS':
+                return resp
+
+            h = resp.headers
+
+            h['Access-Control-Allow-Origin'] = origin
+            h['Access-Control-Allow-Methods'] = get_methods()
+            h['Access-Control-Max-Age'] = str(max_age)
+            if headers is not None:
+                h['Access-Control-Allow-Headers'] = headers
+            return resp
+
+        f.provide_automatic_options = False
+        return update_wrapper(wrapped_function, f)
+    return decorator
+
 
 """
 TODO: Refactor some of this to get rid of the
@@ -173,6 +222,7 @@ GENERAL ROUTES
 
 
 @api.route('/status', methods=['GET'])
+@crossdomain(origin='*')
 def get_status():
     tmp = format_mongo_objs(search.get_status())
     return json.dumps(format_status(tmp))
